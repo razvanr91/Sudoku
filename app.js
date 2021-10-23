ba74df7864d1d2d825d5151427d85d357d802a47
razvanr91 <razvan.roman91@outlook.com>
2021-10-23 12:55:48 +0100

    Work in progress

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ let resolvedSudokuMatrix = [[0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0]];
 
-for(let i = 1; i <= 9; i ++) {
-    for(let j = 1; j <= 9; j++) {
-        sudokuMatrix[i[j]] = j;
-        document.getElementById(`${i}${j}`).innerHTML = sudokuMatrix[i[j]];
-    }
-}
 
 // for(let x = 0; x < 9; x++) {
 //     for(let i = 0; i < 3; i++) {
@@ -45,18 +39,43 @@ for(let i = 1; i <= 9; i ++) {
 // }
 
 
+function checkForDuplicatesOnRow(number, square) {
+    if(number === sudokuMatrix[square+1[0]] || number === sudokuMatrix[square+1[1]] || number === sudokuMatrix[square+1[2]]) {
+        return true;
+    }
+    return false;
+}
+
 
 function generateRandomBoard() {
-    for(let i = 0; i < 9; i++) {
+    let arraysGenerated = 0;
+    let currentSquare = 1;
+    let allNumbers = [];
+    let duplicate = false;
+    while(currentSquare <= 9) {
         let array = randomArray();
-        for(let j = 0; j < 9; j++) {
-            sudokuMatrix[i[j]] = array[j];
-            document.getElementById(`${i+1}${j+1}`).innerHTML = sudokuMatrix[i[j]];
+        if(arraysGenerated === 0) {
+            arraysGenerated++;
+            for(let j = 0; j < 9; j++) {
+                sudokuMatrix[currentSquare[j]] = array[j];
+                document.getElementById(`${currentSquare}${j+1}`).innerHTML = sudokuMatrix[currentSquare[j]];
+                allNumbers.push(array[j]);
+            }
+            ++currentSquare;
+        } else {
+            for(let i = 1; i <= 9; i++) {
+                sudokuMatrix[currentSquare[i]] = array[i-1];
+                document.getElementById(`${currentSquare}${i}`).innerHTML = sudokuMatrix[currentSquare[i-1]];
+            }
+            arraysGenerated++;
+            currentSquare++;
         }
     }
 }
 
 generateRandomBoard();
+
+
 
 function randomArray() {
     let randomArray = [];
